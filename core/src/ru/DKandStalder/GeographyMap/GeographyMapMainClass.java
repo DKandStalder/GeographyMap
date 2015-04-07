65b44bbbbb41c92f1050e5ef00288a464482e4b1
delphikettle <delphikettle@gmail.com>
2015-04-07 19:54:44 +0300

    15_04_06_19_50 //created blank classes for interface and map

--- core/src/ru/DKandStalder/GeographyMap/GeographyMapMainClass.java
+++ core/src/ru/DKandStalder/GeographyMap/GeographyMapMainClass.java
@@ -1,27 +1,50 @@
 package ru.DKandStalder.GeographyMap;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import ru.DKandStalder.GeographyMap.InterfaceView.InterfaceScreen;
+import ru.DKandStalder.GeographyMap.MapView.MapScreen;
 
-public class GeographyMapMainClass extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class GeographyMapMainClass implements ApplicationListener {
+	private MapScreen mapScreen;
+	private InterfaceScreen interfaceScreen;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		this.mapScreen=new MapScreen();
+		this.interfaceScreen=new InterfaceScreen();
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		this.mapScreen.render(Gdx.graphics.getDeltaTime());
+		this.interfaceScreen.render(Gdx.graphics.getDeltaTime());
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
 	}
 }
---
+++ core/src/ru/DKandStalder/GeographyMap/InterfaceView/InterfaceScreen.java
@@ -0,0 +1,43 @@
+package ru.DKandStalder.GeographyMap.InterfaceView;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * Created by DK on 07.04.2015.
+ */
+public class InterfaceScreen implements Screen {
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
