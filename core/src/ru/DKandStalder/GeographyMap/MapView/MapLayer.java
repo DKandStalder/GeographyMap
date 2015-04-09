6efba4c9c7447d35bd7fdd0875af6e7323ddccf8
delphikettle <delphikettle@gmail.com>
2015-04-09 22:32:00 +0300

    15_04_08_22_27 //zooming complete

--- core/src/ru/DKandStalder/GeographyMap/MapView/MapLayer.java
+++ core/src/ru/DKandStalder/GeographyMap/MapView/MapLayer.java
@@ -15,16 +15,15 @@ public class MapLayer extends Actor {
     private TextureRegion textureRegion;
     public MapLayer(int index){
         this.textureRegion=new TextureRegion(new Texture("images/"+index+".png"));
-        this.setColor(new Color(MathUtils.random(0,1),MathUtils.random(0,1),MathUtils.random(0,1),MathUtils.random(0,1)));
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
         Color parentColor=batch.getColor();
+        this.setColor(new Color(MathUtils.random(0,1),MathUtils.random(0, 1), MathUtils.random(0, 1), MathUtils.random(0,1)));
         batch.setColor(this.getColor());
         batch.draw(textureRegion, this.getX(),this.getY());
         batch.setColor(parentColor);
-
     }
 }
--- core/src/ru/DKandStalder/GeographyMap/MapView/MapScreen.java
+++ core/src/ru/DKandStalder/GeographyMap/MapView/MapScreen.java
@@ -2,6 +2,8 @@ package ru.DKandStalder.GeographyMap.MapView;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 
 /**
@@ -9,12 +11,18 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
  */
 public class MapScreen implements Screen {
     private Stage mapStage;
+    private OrthographicCamera camera;
+    private float zoom=1;
+
+    private float initialScale=1;
     public MapScreen(){
         this.mapStage=new Stage();
+        this.camera=new OrthographicCamera(Gdx.graphics.getWidth(),Gdx.graphics.getHeight());
     }
     @Override
     public void show() {
         this.mapStage=new Stage();
+        this.mapStage.getViewport().setCamera(camera);
         for (int i = 0; i < 6; i++) {
             this.mapStage.addActor(new MapLayer(i));
         }
@@ -23,6 +31,7 @@ public class MapScreen implements Screen {
     @Override
     public void render(float delta) {
         this.mapStage.draw();
+        this.camera.zoom=this.zoom;
     }
 
     @Override
@@ -48,5 +57,23 @@ public class MapScreen implements Screen {
     @Override
     public void dispose() {
 
+    }
+    public boolean zoom(float initialDistance, float distance) {
+
+        //Clamp range and set zoom
+        this.zoom = MathUtils.clamp(initialScale * initialDistance / distance, 0.001f, 1000.0f);
+        Gdx.app.log("zoom",zoom+"");
+        return true;
+    }
+    public void setInitialScale(float initialScale) {
+        this.initialScale = initialScale;
+        Gdx.app.log("initialScale",initialScale+"");
+    }
+    public void drag(float dx,float dy){
+        this.camera.position.add(dx*camera.viewportWidth/ Gdx.graphics.getWidth()*zoom,dy*camera.viewportHeight/Gdx.graphics.getHeight()*zoom,0);
+        Gdx.app.log("drag",dx+" "+dy);
+    }
+    public float getCameraZoom(){
+        return this.camera.zoom;
     }
 }
